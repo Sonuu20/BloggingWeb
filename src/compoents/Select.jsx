5ba95328e709e28cf9dffd59f1849ed804514465
ShreyaGupta1234 <guptashreya627@gmail.com>
2023-12-29 22:07:26 +0530

    Select component classes fixed

--- src/compoents/Select.jsx
+++ src/compoents/Select.jsx
@@ -1,25 +1,27 @@
-import React, {useId} from 'react'
+import React from 'react'
 
-const Select = React.forwardRef(function Select({
-    options, //options usally ek array hi millta h
+function Select({
+    options,
     label,
-    className = '',
+    className,
     ...props
 }, ref) {
-    const id = useId()
     return (
-        <div className='w-full'>
-            {label && <label className='' htmlFor={id}></label>}
-            <select {...props} id={id} ref={ref} 
-            className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}>
-                {options?.map((option) => {
-                    <option key={option} value={option}>
-                        {option}
-                    </option>
-                })}
-            </select>
-        </div>
+      <div className='w-full'>
+          {label && <label className=''></label>}
+          <select
+          {...props}
+          ref={ref}
+          className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
+          >
+              {options?.map((option) => (
+                  <option key={option} value={option}>
+                      {option}
+                  </option>
+              ))}
+          </select>
+      </div>
     )
-})
+}
 
-export default Select
+export default React.forwardRef(Select)
