bdba849cd633dc9306b8c33a5e3ffa3afa7e5c54
ShreyaGupta1234 <guptashreya627@gmail.com>
2023-12-31 14:17:18 +0530

    typoError fixed

--- src/compoents/PostCard.jsx
+++ src/compoents/PostCard.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import service from '../appwrite/conf'
 import { Link } from 'react-router-dom'
 
-function PostCard({$id, title, Image}) {
+function PostCard({$id, title, image}) {
   return (
     <Link to={`/post/${$id}`}>
         <div className='w-full bg-gray-100 rounded-xl p-4'>
             <div className='w-full justify-center mb-4'>
-                <img src={service.getFilePreview(Image)} alt={title} />
+                <img src={service.getFilePreview(image)} alt={title} />
             </div>
             <h2 className='text-xl font-bold'>{title}</h2>
         </div>
