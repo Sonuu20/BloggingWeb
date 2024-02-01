987e953763c90882b0faf67b6f4ec8b45170dcf3
ShreyaGupta1234 <guptashreya627@gmail.com>
2024-02-01 07:52:22 +0530

    fixed css classes

--- src/compoents/PostCard.jsx
+++ src/compoents/PostCard.jsx
@@ -5,9 +5,9 @@ import { Link } from 'react-router-dom'
 function PostCard({$id, title, image}) {
   return (
     <Link to={`/post/${$id}`}>
-        <div className='w-full bg-gray-100 rounded-xl p-4'>
+        <div className='w-full bg-gray-100 rounded-xl p-4 min-w-56'>
             <div className='w-full justify-center mb-4'>
-                <img src={service.getFilePreview(image)} alt={title} />
+                <img src={service.getFilePreview(image)} alt={title} className='rounded-xl' />
             </div>
             <h2 className='text-xl font-bold'>{title}</h2>
         </div>
