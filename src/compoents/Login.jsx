922121140d8aa76b2b002f28136bc730b07b3d2c
ShreyaGupta1234 <guptashreya627@gmail.com>
2024-02-04 07:59:24 +0530

    Fixed alignment

--- src/compoents/Login.jsx
+++ src/compoents/Login.jsx
@@ -27,8 +27,8 @@ function Login() {
         }
     }
   return (
-    <div className='flex items-center justify-center w-full'>
-        <div className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}>
+    <div className='flex items-center justify-center w-full '>
+        <div className={` w-full max-w-lg mx-2 bg-gray-100 rounded-xl p-10 border border-black/10`}>
             <div className="mb-2 flex justify-center">
                 <span className="inline-block w-full max-w-[100px]">
                     <Logo width="100%" />
