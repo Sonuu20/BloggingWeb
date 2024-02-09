f814d126f08acf2edfaf52b1fcca35d609482565
ShreyaGupta1234 <guptashreya627@gmail.com>
2024-02-09 07:47:52 +0530

    changed bg color

--- src/compoents/Header/LogoutBtn.jsx
+++ src/compoents/Header/LogoutBtn.jsx
@@ -13,7 +13,7 @@ function LogoutBtn() {
         })
     }
   return (
-    <button className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full' onClick={LogoutHandler}>Logout</button>
+    <button className='inline-block text-gray-200 px-6 py-2 duration-200 hover:bg-blue-100 hover:text-black rounded-full' onClick={LogoutHandler}>Logout</button>
   )
 }
 
