45ae14bdcfa02ba362892be53ea439cb6f30832b
ShreyaGupta1234 <guptashreya627@gmail.com>
2024-02-12 09:41:54 +0530

    fixed config files

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      minWidth: {
+        '56': '9rem'
+      }
+    },
   },
   plugins: [],
 }
