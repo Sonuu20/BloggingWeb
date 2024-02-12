fc27913a53d63ccac24279cd57e9385fdcf8778f
ShreyaGupta1234 <guptashreya627@gmail.com>
2024-02-12 09:30:20 +0530

    fixed some css & added authorName

--- src/compoents/PostCard.jsx
+++ src/compoents/PostCard.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import service from '../appwrite/conf'
 import { Link } from 'react-router-dom'
 
-function PostCard({$id, title, image}) {
+function PostCard({$id, title, image, authorName}) {
   return (
     <Link to={`/post/${$id}`}>
-        <div className='w-full bg-gray-100 rounded-xl p-4 min-w-56'>
-            <div className='w-full justify-center mb-4'>
-                <img src={service.getFilePreview(image)} alt={title} className='rounded-xl' />
+        <div className='w-full card-hover rounded-xl p-4  bg-gray-700'>
+            <div className='w-full justify-center mb-4 h-[80%]'>
+                <img src={service.getFilePreview(image)} alt={title} className='rounded-xl object-cover' />
             </div>
             <h2 className='text-xl font-bold'>{title}</h2>
+            <h2 className='text-xl font-bold'>{authorName}</h2>
+
         </div>
     </Link>
   )
