32465cec594fb61a85c69aefc3df5b9b0b35a777
ShreyaGupta1234 <guptashreya627@gmail.com>
2024-02-12 09:22:47 +0530

    set authentication deafult=true

--- src/compoents/AuthLayout.jsx
+++ src/compoents/AuthLayout.jsx
@@ -2,10 +2,12 @@ import React, {useState, useEffect} from 'react'
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 
-export default function Protected({children, authentication}) {
+export  function Protected({children, authentication = true}) {
+
     const navigate = useNavigate()
     const [loading, setLoading] = useState(true)
     const authStatus = useSelector(state => state.auth.status)
+    
     useEffect(() => {
         if(authentication && authStatus !== authentication){
             navigate("/login")
