59e854c5eee092cdf67cd2bf60f0a0e7942ce308
ShreyaGupta1234 <guptashreya627@gmail.com>
2024-03-09 10:55:51 +0530

    added some css classes

--- src/compoents/PostCard.jsx
+++ src/compoents/PostCard.jsx
@@ -9,8 +9,8 @@ function PostCard({$id, title, image, authorName}) {
             <div className='w-full justify-center mb-4 h-[80%]'>
                 <img src={service.getFilePreview(image)} alt={title} className='rounded-xl object-cover' />
             </div>
-            <h2 className='text-xl font-bold'>{title}</h2>
-            <h2 className='text-xl font-bold'>{authorName}</h2>
+            <h2 className='text-xl font-bold text-white'>{title}</h2>
+            <h2 className=' text-base font-bold text-white'>Auhtor: {authorName}</h2>
 
         </div>
     </Link>
